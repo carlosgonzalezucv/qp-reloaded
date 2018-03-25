b16b055626778d6026c1e969e8ce730059ed8d22
Carlos Luis González <carlosl.gonzalez.ucv@gmail.com>
2018-03-25 18:10:32 -0400

    Segundo commit

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -10,17 +10,7 @@
         abstract: true,
         url: "/index",
         templateUrl: "app/components/common/content.html"
-      })
-      .state('index.main', {
-        url: "/main",
-        templateUrl: "app/main/main.html",
-        data: { pageTitle: 'Example view' }
-      })
-      .state('index.minor', {
-        url: "/minor",
-        templateUrl: "app/minor/minor.html",
-        data: { pageTitle: 'Example view' }
       });
-    $urlRouterProvider.otherwise('/index/main');
+    $urlRouterProvider.otherwise('/index');
   }
 })();
