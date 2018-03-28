1d5cae36fd4841bacb25e775438bc658281dd92f
carlosgonzalezucv <carlosl.gonzalez.ucv@gmail.com>
2018-03-27 22:07:41 -0400

    Quitando ecs2015

--- src/app/directives/critic-level/critic-level.directive.js
+++ src/app/directives/critic-level/critic-level.directive.js
@@ -11,13 +11,15 @@
         level: "=?",
       },
       controller: function($scope) {
-        let maxRating = 5, { level } = $scope;
-        let remain = maxRating - level;
+        var maxRating = 5, level = $scope.level;
+        var remain = maxRating - level;
         this.stars = new Array(maxRating).fill(0);
-        this.rating = this.stars.map((e, i) => (
-          (i < level) ? '<i class="fa fa-star" style="color: rgb(255,128,123)"></i>': 
+        this.rating = this.stars.map(function(e, i){ 
+          return (
+            (i < level) ? '<i class="fa fa-star" style="color: rgb(255,128,123)"></i>': 
                         '<i class="fa fa-star" style="color: rgba(255,128,123,.4)"></i>'
-        )).join('')
+          )
+        }).join('');
       },
       controllerAs: 'vm'
     };
