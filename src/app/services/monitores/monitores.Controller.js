339befc17cb0ea1f5b303efa2e68461bc1715e8e
Unknown <carlosl.gonzalez.ucv@gmail.com>
2018-03-30 20:41:02 -0400

    Adaptacion
    Se adapta la vista para trabajar con la data desde el 
    controlador

--- src/app/services/monitores/monitores.Controller.js
+++ src/app/services/monitores/monitores.Controller.js
@@ -1,14 +1,34 @@
 (function () {
-    'use strict';  
-    angular
-      .module('inspinia')
-      .controller('MonitoresController', [
-        '$state', 
-        PerformanceCtrl
-      ]);  
-    /** @ngInject */
-    function PerformanceCtrl($state) {
-      var vm = this;
-      $state.go('index.monitores.general')
-    }  
-  })();
+  'use strict';  
+  angular
+    .module('inspinia')
+    .controller('MonitoresController', [
+      '$state', 
+      PerformanceCtrl
+    ]);  
+  /** @ngInject */
+  function PerformanceCtrl($state) {
+    var vm = this, baseState = "index.monitores", className = "col-md-2 col-sm-2 col-xs-4";
+    vm.cName = className;
+    vm.navButtons = [{
+      title: "INF_GENERAL",
+      target: baseState + ".general"
+    },{
+      title: "SERVIDORES",
+      target: baseState + ".servidor",
+    },{
+      title: "LISTA_NEGRA",
+      target: baseState + ".blacklist",
+    },{
+      title: "CORREOS",
+      target: baseState + ".correos",
+    },{
+      title: "DNS",
+      target: baseState + ".dns",
+    },{
+      title: "CONECTIVIDAD",
+      target: baseState + ".conectividad",
+    }];
+    $state.go('index.monitores.general')
+  }  
+})();
